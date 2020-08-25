f956dd2b64bc029d77237cf978f1cdebfab6e11f
Fabian Gans <fgans@bgc-jena.mpg.de>
2020-08-25 17:36:06 +0200

    Make NetCDF dataset not return handles

--- src/datasets/netcdf.jl
+++ src/datasets/netcdf.jl
@@ -16,11 +16,31 @@ struct NetCDFDataset
 end
 NetCDFDataset(filename) = NetCDFDataset(filename,NC_NOWRITE)
 
+import .NetCDF: AbstractDiskArray, readblock!, writeblock!, haschunks, eachchunk
+
+struct NetCDFVariable{T,N} <: AbstractDiskArray{T,N}
+  filename::String
+  varname::String
+  size::NTuple{N,Int}
+end
+#Define method forwarding for DiskArray methods
+for m in [:haschunks, :eachchunk]
+  eval(:(function $(m)(v::NetCDFVariable,args...;kwargs...)
+    NetCDF.open(a->$(m)(a,args...;kwargs...), v.filename, v.varname)
+end
+))
+end
+writeblock!(v::NetCDFVariable, aout, r::AbstractUnitRange...) = NetCDF.open(a->writeblock!(a,aout,r...), v.filename, v.varname, mode=NC_WRITE)
+readblock!(v::NetCDFVariable, aout, r::AbstractUnitRange...) = NetCDF.open(a->readblock!(a,aout,r...), v.filename, v.varname)
+
+Base.size(v::NetCDFVariable) = v.size
+
 get_var_dims(ds::NetCDFDataset,name) = NetCDF.open(v->map(i->i.name,v[name].dim),ds.filename)
 get_varnames(ds::NetCDFDataset) = NetCDF.open(v->collect(keys(v.vars)),ds.filename)
 get_var_attrs(ds::NetCDFDataset, name) = NetCDF.open(v->v[name].atts,ds.filename)
 function Base.getindex(ds::NetCDFDataset, i)
-  NetCDF.open(ds.filename,i,mode=ds.mode)
+  s,et = NetCDF.open(j->(size(j),eltype(j)),ds.filename,i)
+  NetCDFVariable{et,length(s)}(ds.filename, i, s)
 end
 Base.haskey(ds::NetCDFDataset,k) = NetCDF.open(nc->haskey(nc.vars,k),ds.filename)
 
@@ -28,12 +48,12 @@ function add_var(p::NetCDFDataset, T::Type, varname, s, dimnames, attr;
   chunksize=s, compress = -1)
   dimsdescr = Iterators.flatten(zip(dimnames,s))
   nccreate(p.filename, varname, dimsdescr..., atts = attr, t=T, chunksize=chunksize, compress=compress)
-  NetCDF.open(p.filename,varname,mode=p.mode)
+  NetCDFVariable{T,length(s)}(p.filename,varname,s)
 end
 
 function create_empty(::Type{NetCDFDataset}, path)
   NetCDF.create(path, NcVar[])
-  NetCDFDataset(path,NC_WRITE)
+  NetCDFDataset(path)
 end
 
 allow_parallel_write(::NetCDFDataset) = false
