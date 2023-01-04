01d5f26e6520b22d800556d30c65bbd529bf5912
Fabian Gans <fgans@bgc-jena.mpg.de>
2023-01-04 16:11:23 +0100

    fix axisindices

--- src/axisarrays/axisindices.jl
+++ src/axisarrays/axisindices.jl
@@ -2,7 +2,7 @@ using .AxisIndices: AbstractAxis,AxisIndices
 
 valfromaxis(ax::AbstractAxis) = keys(ax)
 
-getdata(a::AxisIndices.AxisArray) = parent(a)
+getdata(a::AxisIndices.AxisIndicesArray) = parent(a)
 
-yaxcreate(::Type{<:AxisIndices.AxisArray}, data, dnames, dvals, atts) =
-  AxisIndices.AxisArray(data, map(i->dvals[i], 1:ndims(data))...)
+yaxcreate(::Type{<:AxisIndices.AxisIndicesArray}, data, dnames, dvals, atts) =
+  AxisIndices.AxisIndicesArray(data, (dvals...,))
