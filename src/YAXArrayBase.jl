de13de64666447376761d9503dfe1ac0885797e0
Felix Cremer <fcremer@bgc-jena.mpg.de>
2023-01-28 21:50:24 +0100

    Remove the Requires setup for lower julia versions
    We decided to only support julia versions larger than 1.9

--- src/YAXArrayBase.jl
+++ src/YAXArrayBase.jl
@@ -1,7 +1,5 @@
 module YAXArrayBase
-#if !isdefined(Base, :get_extension)
-#  using Requires
-#end
+
   using DataStructures: OrderedDict
 
 include("datasets/datasetinterface.jl")
@@ -24,32 +22,6 @@ function __init__()
   )
 
   backendregex = Pair[]
-  #=
-@static if !isdefined(Base, :get_extension)
-  @require NamedDims="356022a1-0364-5f58-8944-0da4b18d706f" include("axisarrays/nameddims.jl")
-
-  @require DimensionalData="0703355e-b756-11e9-17c0-8b28908087d0" include("axisarrays/dimensionaldata.jl")
-
-  @require AxisArrays="39de3d68-74b9-583c-8d2d-e117c070f3a9" include("axisarrays/axisarrays.jl")
-
-  @require AxisIndices="f52c9ee2-1b1c-4fd8-8546-6350938c7f11" include("axisarrays/axisindices.jl")
-
-  @require AxisKeys="94b1ba4f-4ee9-5380-92f1-94cde586c3c5" include("axisarrays/axiskeys.jl")
-
-  @require ArchGDAL="c9ce4bd3-c3d5-55b8-8973-c0e20141b8c3" begin 
-    include("axisarrays/archgdal.jl")
-    include("datasets/archgdal.jl")
-  end
-
-
-  @require Zarr="0a941bbe-ad1d-11e8-39d9-ab76183a1d99" include("datasets/zarr.jl")
-
-  @require NetCDF="30363a11-5582-574a-97bb-aa9a979735b9" include("datasets/netcdf.jl")
-
-end
-=#
-
-
 end
 
 
